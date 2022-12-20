7af2439c03d96ccd46e4308750c888df51f798d4
Imran khan <adhurekhavab@gmail.com>
2022-12-20 17:21:25 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const getMovieRequest = async () => {
+    const url = "http://www."
+    const data = await Response.json();
+  }
+  return (
+    <>
+      <div className="movie-app">
+        <h1> movie list</h1>
+        <movielist  > </movielist>
+      </div>
+    </>
+
+  );
+}
+
+export default App;
